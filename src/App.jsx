85251617c557a02aa07b51ecdc5bf48a7b0bd49c
lucasvt22 <lucas22@vt.edu>
2024-04-11 16:18:54 -0400

    Create App and add funtionality/Links to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom'
+import Gallery from './pages/Gallery'
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import { Link } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => { 
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/",
+      element:<Home />
+    },
+    {
+      path: "/crew",
+      element:<Gallery />
+    },
+    {
+      path:"/crew/edit/:id",
+      element: <EditPost />
+    },
+    {
+      path:"/new",
+      element: <CreatePost />
+    }
+  ]);
+
+  return ( 
+
+    <div className="App">
+      <div className="nav">
+        <h1>Fallout Crews</h1>
+        <Link to="/"><button className="navBtn"> Home </button></Link>
+        <Link to="/crew"><button className="navBtn"> Explore Survivors </button></Link>
+        <Link to="/new"><button className="navBtn"> Create Survivor </button></Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        {element}
+    </div>
+
+  );
 }
 
-export default App
+export default App;
