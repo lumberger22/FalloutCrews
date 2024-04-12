c0459b4dcd0d34c10b97a650c9bb5be10b4258a5
lucasvt22 <lucas22@vt.edu>
2024-04-12 11:07:59 -0400

    Create Home page layout and add nav buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
+import './Home.css';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Fallout Crews</h1>
+    <div className='home--page'>
+      <h1 className="title">Fallout Crews</h1>
+      <h3 className="subtitle">Welcome to the Wasteland!</h3>
+      <p className="description">Create, manage, and strategize with your personalized crew of end-world survivors to navigate a post-apocalyptic world</p>
+      <div className="btn--container">
+        <Link to="/crew"><button className="navBtn"> Explore Survivors </button></Link>
+        <Link to="/new"><button className="navBtn"> Create Survivor </button></Link>
+        </div>
     </div>
   );
 }
